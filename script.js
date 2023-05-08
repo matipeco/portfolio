41c133284f0376333682d44ce52d8749210be192
matipeco <matiaspecorale1995@gmail.com>
2023-05-08 20:19:26 -0300

    actualizando script cambiar clase

--- script.js
+++ script.js
@@ -4,10 +4,52 @@
 //rootMargin, margen alrededor del elemento seleccionado en el root, por defecto es todo 0
 //El threshold, indica a que porcentaje de visibilidad del elemento target, la funcion del observer deberia ser ejecutada.
 
+// const navLinks = document.querySelectorAll(".navbar__list-item a");
+// const socialLinks = document.querySelectorAll(".social__list-item a");
+
+// const cambiarClase = (entries, observer) => {
+//   console.log(entries);
+//   entries.forEach((entry) => {
+//     if (entry.isIntersecting && entry.intersectionRatio >= 0.8) {
+//       navLinks.forEach((link) => {
+//         link.classList.add("white-text", "white-border");
+//       });
+//       socialLinks.forEach((link) => {
+//         link.style.color = "#82df8d";
+//       });
+//     } else if (!entry.isIntersecting && entry.intersectionRatio <= 0.8) {
+//       navLinks.forEach((link) => {
+//         link.classList.remove("white-text", "white-border");
+//       });
+//       socialLinks.forEach((link) => {
+//         link.style.color = "rgba(11, 11, 11, 0.7)";
+//       });
+//     }
+//   });
+// };
+
+// const options = {
+//   threshold: [0, 0.8],
+// };
+
+// const observarSeccion = (id) => {
+//   const section = document.querySelector(`#${id}`);
+//   let observer = new IntersectionObserver(
+//     (entries) => cambiarClase(entries, observer),
+//     options
+//   );
+//   observer.observe(section);
+// };
+
+// observarSeccion("sobre-mi");
+// observarSeccion("proyectos");
+// observarSeccion("contacto");
+
 const navLinks = document.querySelectorAll(".navbar__list-item a");
 const socialLinks = document.querySelectorAll(".social__list-item a");
 
 const cambiarClase = (entries, observer) => {
+  console.log(entries);
   entries.forEach((entry) => {
     if (entry.isIntersecting && entry.intersectionRatio >= 0.8) {
       navLinks.forEach((link) => {
@@ -16,6 +58,16 @@ const cambiarClase = (entries, observer) => {
       socialLinks.forEach((link) => {
         link.style.color = "#82df8d";
       });
+      if (entry.target.id === "contacto") {
+        // seccion de contacto
+        navLinks.forEach((link) => {
+          link.classList.remove("white-text");
+          link.classList.add("black-text");
+        });
+        socialLinks.forEach((link) => {
+          link.style.color = "black";
+        });
+      }
     } else if (!entry.isIntersecting && entry.intersectionRatio <= 0.8) {
       navLinks.forEach((link) => {
         link.classList.remove("white-text", "white-border");
@@ -23,6 +75,36 @@ const cambiarClase = (entries, observer) => {
       socialLinks.forEach((link) => {
         link.style.color = "rgba(11, 11, 11, 0.7)";
       });
+      if (entry.target.id === "contacto") {
+        // seccion de contacto
+        navLinks.forEach((link) => {
+          link.classList.add("white-text");
+          link.classList.remove("black-text");
+        });
+        socialLinks.forEach((link) => {
+          link.style.color = "rgba(255, 255, 255, 0.7)";
+        });
+      }
+      if (entry.target.id === "header") {
+        // seccion de contacto
+        navLinks.forEach((link) => {
+          link.classList.add("white-text");
+          link.classList.remove("black-text");
+        });
+        socialLinks.forEach((link) => {
+          link.style.color = "rgba(255, 255, 255, 0.7)";
+        });
+      }
+      if (entry.target.id === "habilidades") {
+        // seccion de contacto
+        navLinks.forEach((link) => {
+          link.classList.add("white-text");
+          link.classList.remove("black-text");
+        });
+        socialLinks.forEach((link) => {
+          link.style.color = "rgba(255, 255, 255, 0.7)";
+        });
+      }
     }
   });
 };
@@ -42,6 +124,7 @@ const observarSeccion = (id) => {
 
 observarSeccion("sobre-mi");
 observarSeccion("proyectos");
+observarSeccion("contacto");
 
 const btn = document.getElementById("button");
 const inputs = form.querySelectorAll("input, textarea");
